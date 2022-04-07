02a9c56eedfb50acc25952e9492e919ac42f7d10
Daniel Wagner <wagnerd58@gmail.com>
2022-04-07 14:18:32 -0600

    Time Dependencies: Added fun calc_RevisionRisk
    Added function to estimate time-dependent transition probability for risk of revision following primary THR.

--- src/FUNS/Model-Parameters_Time-Dependencies.R
+++ src/FUNS/Model-Parameters_Time-Dependencies.R
@@ -19,7 +19,58 @@ calc_MR <- function(LT, Age0 = 60, nCycles = 60) {
   return(MR)
 }
 
-# Modify Parameter List: Insert Time-Dependencies
+
+
+# Calc_RevisionRisk() ==========================================================
+calc_RevisionRisk <- function(Survival, 
+                              Age0 = 60, 
+                              nCycles = 60) {
+  # Survival model fitted to capture intervention (STD vs NP1) and time-to-event. 
+  #   Event: Prosthesis Failure. 
+  #   Coefficients are on the log-scale, so hazard rate can be obtained by 
+  #   exponentiating the coefficients. 
+  Survival <- cbind(coef = Survival[,"coef"], 
+                    HR = exp(Survival[,"coef"]))
+  # Estimate Baseline Hazard: 
+  ## calculate lambda and gamma from Weibull distribution. 
+  ##    - lambda: The log of the lambda parameter is a linear sum of the 
+  ##      coefficients multiplied by the explanatory variables in the model. 
+  Lambda <- sapply(X = list(Male = 1, 
+                            Female = 0), 
+                   FUN = \(x){
+                     c(cons = 1, Age = Age0, male = x) * 
+                       Survival[c("cons", "age", "male"), "coef"]
+                   })
+  Lambda <- colSums(x = Lambda, na.rm = FALSE, dims = 1)
+  Lambda <- exp(Lambda)
+  
+  ##    - gamma: exponentiate the ln.gamma coefficient in the survival model. 
+  GMMA <- Survival["ln.gamma","HR"]
+  
+  # Estimate the Relative Risk of Revision for each Prosthesis Type
+  jNames <- grep(pattern = "NP", x = rownames(Survival), value = TRUE)
+  rr <- c(1, Survival[jNames, "HR"])
+  names(rr) <- c("STD", jNames)
+  
+  ## Estimate time-dependency
+  Cycle <- 1:nCycles
+  
+  RevisionRisk <- 
+    sapply(X = Lambda, 
+           FUN = \(sex){
+             sapply(X = rr, 
+                    FUN = \(risk){
+                      1 - exp(sex * rr * (((Cycle - 1)^GMMA) - (Cycle ^ GMMA)))
+                    })
+           }, simplify = "array")
+  
+  names(dimnames(RevisionRisk)) <- c("Cycle", "j", "Gender")
+  
+  # Return Output
+  return(RevisionRisk)
+}
+
+# Modify Parameter List: Insert Time-Dependencies ==============================
 calc_TimeDeps <- function(ParamList, 
                           Age0 = 60, 
                           nCycles = 60) {
@@ -28,8 +79,14 @@ calc_TimeDeps <- function(ParamList,
                           Age0 = Age0, 
                           nCycles = nCycles)
   
+  # Revision Risk stratified by Age and Gender
+  ParamList$RevisionRisk <- 
+    calc_RevisionRisk(Survival = ParamList$Survival$Survival, 
+                      Age0 = Age0, 
+                      nCycles = nCycles)
+  
   # Arrange List, drop un-necessary list-elements: LifeTables
-  ParamList <- ParamList[c("OMR", "RRR", "MR", "Survival")]
+  ParamList <- ParamList[c("OMR", "RRR", "MR", "RevisionRisk")]
   
   
   return(ParamList)
