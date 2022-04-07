f877c9e2ccfea2880109e3123c86044f068fe517
Daniel Wagner <wagnerd58@gmail.com>
2022-04-07 10:35:00 -0600

    Added Function: getParams()
    Checks if model parameters is available. If yes, shows path. If no, prepares list from raw data.

---
+++ src/FUNS/Model-Parameters.R
@@ -0,0 +1,55 @@
+getParams <- function(File = "THR-Params.rds") {
+  Params.Dir <- file.path("data", "data-gen", "Model-Params")
+  
+  if (isFALSE(dir.exists(Params.Dir))) {
+    usethis::ui_oops("Missing {usethis::ui_path(Params.Dir)} sub-directory!")
+    
+    dir.create(path = Params.Dir)
+    
+    usethis::ui_done("Created {usethis::ui_path(Params.Dir)} sub-directory.")
+  }
+  
+  Dir.Content <- list.files(path = Params.Dir)
+  
+  if (length(Dir.Content) == 0) {
+    usethis::ui_info("Model Parameters have not been generated!")
+    usethis::ui_info("Preparing list from raw data")
+    
+    # Life Tables: Age & Gender Stratified Risk of Death
+    LT <- readr::read_rds(file.path("data", 
+                                    "data-raw", 
+                                    "Life-Tables.rds"))
+    
+    # Survival: Revision Risk following Primary THR. 
+    SurvFit <- readr::read_rds(file.path("data", 
+                                         "data-raw", 
+                                         "THR-Survival.rds"))
+    
+    # Operative Mortality Rate
+    ## Primary and Revision Assumed equal to 0.02
+    OMR <- c(PRI = 0.02, REV = 0.02)
+    
+    # Re-Revision Risk
+    RRR <- 0.04
+    
+    # Combine into List: 
+    THR_Params <- 
+      list(OMR = OMR, 
+           RRR = RRR, 
+           LifeTables = LT, 
+           Survival = SurvFit)
+    
+    ## write to data-gen/Model-Params
+    Param.Path <- file.path(Params.Dir, File)
+    readr::write_rds(x = THR_Params, 
+                     file = Param.Path)
+    
+    usethis::ui_done("{usethis::ui_field('THR_Params')} saved to {usethis::ui_path(Param.Path)}")
+    
+    
+  } else {
+    Param.Path <- file.path(Params.Dir, File)
+    usethis::ui_info("Load parameters from {usethis::ui_path(Param.Path)}")
+  }
+  
+}
