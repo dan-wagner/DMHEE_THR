4d4ca9e5cce242f38bea22acda50ddb530a77853
Daniel Wagner <wagnerd58@gmail.com>
2022-06-23 16:41:50 -0600

    Run Nested MC Simulations for EVPPI
    Model: STD vs NP1 vs NP2

---
+++ src/02_STD-v-NP1-v-NP2/01_Simulations/03_Nested-Monte-Carlo_EVPPI-OMR.R
@@ -0,0 +1,103 @@
+# Conduct Monte Carlo Simulation of THR Cohort Model
+
+source(file = file.path("src", "FUNS", "Model-Parameters.R"))
+source(file = file.path("src", "FUNS", "Model-Parameters_Time-Dependencies.R"))
+source(file = file.path("src", "FUNS", "Model-Parameters_Draws.R"))
+source(file = file.path("src", "FUNS", "THR-Model.R"))
+
+# Get Model Parameters =========================================================
+getParams(include_NP2 = TRUE)
+THR_Params <- readr::read_rds(file = file.path("data", 
+                                               "data-gen", 
+                                               "Model-Params", 
+                                               "THR-Params_j3.rds"))
+
+# Run Simulation ===============================================================
+PHI <- "OMR"
+
+THR.Age <- seq(from = 40, to = 80, by = 20)
+THR.Gender <- c("Male", "Female")
+names(THR.Age) <- THR.Age
+names(THR.Gender) <- THR.Gender
+
+library(foreach)
+library(doParallel)
+registerDoParallel(cores = 6)
+
+Sim.Start <- Sys.time()
+
+NMCOut <- 
+  foreach(n = 1:1000, 
+          .final = simplify2array) %dopar% {
+    # Draw Outer Loop Parameter
+    PHI_i <- DrawParams(ParamList = THR_Params, prob = 1)[PHI]
+    
+    
+    replicate(n = 1000, 
+              expr = {
+                PSI_i <- DrawParams(ParamList = THR_Params, prob = 1)
+                PSI_i[PHI] <- PHI_i
+                
+                Result <- 
+                  sapply(X = THR.Age, 
+                         FUN = \(age){
+                           sapply(X = THR.Gender, 
+                                  FUN = \(sex){
+                                    sapply(X = list(STD = "STD", 
+                                                    NP1 = "NP1", 
+                                                    NP2 = "NP2"), 
+                                           FUN = runModel, 
+                                           ParamList = PSI_i, 
+                                           Age0 = age, 
+                                           Gender = sex, 
+                                           nCycles = 60, 
+                                           cDR = 0.06, 
+                                           oDR = 0.015, 
+                                           simplify = "array")
+                                  }, 
+                                  simplify = "array")
+                         }, 
+                         simplify = "array")
+                
+                names(dimnames(Result)) <- c("Results", "j", "Gender", "Age")
+                Result
+              })
+  }
+
+Sim.Stop <- Sys.time()
+
+stopCluster()
+
+str(NMCOut)
+
+NoDimName <- which(names(dimnames(NMCOut)) == "")
+
+names(dimnames(NMCOut))[NoDimName] <- c("PSI", "PHI") 
+NMCOut <- aperm(a = NMCOut, perm = c("PSI", "Results", "j", "PHI", "Gender", "Age"))
+
+Sim.Stop - Sim.Start
+
+## Save Output to data-gen 
+names(THR.Gender) <- c("M", "F")
+
+for (sex in seq_along(THR.Gender)) {
+  for (age in seq_along(THR.Age)) {
+    
+    Result.Data <- NMCOut[,,,,THR.Gender[[sex]],names(THR.Age)[[age]]]
+    
+    Result.PATH <- 
+    file.path("data", 
+              "data-gen", 
+              "Simulation-Output", 
+              "02_STD-v-NP1-v-NP2", 
+              paste("Nested", 
+                    "MC-Sim", 
+                    paste0(names(THR.Gender)[sex], 
+                           names(THR.Age)[age]), 
+                    "OMR.rds", sep = "_"))
+    
+    readr::write_rds(x = Result.Data, 
+                     file = Result.PATH)
+    
+  }
+}
