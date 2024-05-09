753c7d817bb63c66245c941fcf8ac54a7dd25a7e
Daniel Wagner <wagnerd58@gmail.com>
2024-05-08 20:58:59 -0600

    Refactored function: getParams
    Saves all parameter data. Subsets based on condition for 2 or 3 alternatives.

--- src/FUNS/Model-Parameters.R
+++ src/FUNS/Model-Parameters.R
@@ -1,83 +1,78 @@
-getParams <- function(include_NP2 = FALSE) {
-  # Set Directory and File Names
-  Params.Dir <- file.path("data", "data-gen", "Model-Params")
-  if (isTRUE(include_NP2)) {
-    File <- "THR-Params_j3.rds"
+getParams <- function(FileName = "params.rds", include_NP2 = FALSE) {
+  # Get Model Parameters
+  #
+  # Args:
+  #   FileName: Character (Default = "params.rds"). The name to give the 
+  #     parameter file.
+  #   include_NP2: Logical (Default = FALSE). Switch to control whether model
+  #     considers 2 (Default) or 3 alternatives. 
+  #
+  # Returns:
+  #   A list with X named elements. 
+  #     {A}. 
+  #     {B}. 
+  #     {C}. 
+  #     {D}. 
+  
+  # Set Path to output file
+  param_path <- file.path("data", "data-gen", "Model-Params", FileName)
+  # Check if File Exists
+  params_exist <- file.exists(param_path)
+  
+  if (isFALSE(params_exist)) {
+    usethis::ui_info(x = "Model Parameters have not been generated!")
+    # Check if sub-directory exists
+    param_dir <- 
+      sub(pattern = "[\\/]params.rds", replacement = "", x = param_path)
+    dir_present <- dir.exists(paths = param_dir)
+    
+    if (isFALSE(dir_present)) {
+      usethis::ui_oops(x = "Missing directory: {usethis::ui_path(param_dir)}")
+      dir.create(path = param_dir)
+      usethis::ui_done(x = "Created directory: {usethis::ui_path(param_dir)}")
+    }
+    # Import the data in data-raw
+    usethis::ui_info("Generating parameters from raw data!")
+    params <- list.files(path = file.path("data", "data-raw"),
+                         pattern = ".rds",
+                         full.names = TRUE)
+    names(params) <- sub(pattern = "data/data-raw/",
+                         replacement = "",
+                         x = params)
+    names(params) <- sub(pattern = ".rds",
+                         replacement = "", 
+                         x = names(params))
+    names(params) <- sub(pattern = "(THR-)|(THR_)|(THR)",
+                         replacement = "",
+                         x = names(params))
+    names(params) <- sub(pattern = "-",
+                         replacement = "",
+                         x = names(params))
+    # Import Data
+    params <- lapply(X = params, FUN = readr::read_rds)
+    # Add Prices
+    params$Prices <- c(STD = 394, NP1 = 579, NP2 = 788)
+    # Write data to param_path
+    readr::write_rds(x = params, file = param_path)
+    usethis::ui_done("Parameter list saved to {usethis::ui_path(param_path)}")
   } else {
-    File <- "THR-Params_j2.rds"
+    params <- readr::read_rds(file = param_path)
+    usethis::ui_info("Loaded parameters from: {usethis::ui_path(param_path)}")
   }
   
-  # Check that DIR Exists
-  if (isFALSE(dir.exists(Params.Dir))) {
-    usethis::ui_oops("Missing sub-directory: {usethis::ui_path(Params.Dir)}")
-    
-    dir.create(path = Params.Dir)
-    
-    usethis::ui_done("Created sub-directory: {usethis::ui_path(Params.Dir)}")
-  }
-  
-  # Check DIR Content
-  Dir.Content <- list.files(Params.Dir)
-  path2File <- file.path(Params.Dir, File)
-  
-  if (isFALSE(File %in% Dir.Content)) {
-    usethis::ui_info("Model Parameters have not been generated!")
-    usethis::ui_info("Preparing list from raw data.")
-    # TRANSITION PROBABILITIES -----------------------------
-    # Life Tables: Age & Gender Stratified Risk of Death
-    LT <- readr::read_rds(file.path("data", 
-                                    "data-raw", 
-                                    "Life-Tables.rds"))
-    # Survival: Revision Risk following Primary THR. 
-    if (isTRUE(include_NP2)) {
-      SurvFit <- readr::read_rds(file.path("data", 
-                                           "data-raw", 
-                                           "THR-Survival_NP2.rds"))
-    } else {
-      SurvFit <- readr::read_rds(file.path("data", 
-                                           "data-raw", 
-                                           "THR-Survival.rds"))
-    }
-    
-    # Operative Mortality Rate
-    ## Primary and Revision Assumed equal to 0.02
-    OMR <- c(PRI = 0.02, REV = 0.02)
-    
-    # Re-Revision Risk
-    RRR <- 0.04
-    
-    # COSTING -----------------------------------------------
-    # Cost of Alternative Protheses
-    Cost_j <- c(STD = 394, NP1 = 579)
-    if (isTRUE(include_NP2)) {
-      Cost_j <- c(Cost_j, NP2 = 788)
-    }
-    
-    # Health State Costs
-    Cost_states <- readr::read_rds(file = file.path("data", 
-                                                    "data-raw", 
-                                                    "THR_Costs_States.rds"))
-    # UTILITIES ---------------------------------------------
-    Utilities <- readr::read_rds(file = file.path("data", 
-                                                  "data-raw", 
-                                                  "THR-Utilities.rds"))
-    
-    # Combine into List: 
-    THR_Params <- 
-      list(OMR = OMR, 
-           RRR = RRR, 
-           LifeTables = LT, 
-           Survival = SurvFit, 
-           Cost_j = Cost_j, 
-           Cost_States = Cost_states, 
-           Utilities = Utilities)
-    
-    # Write to: data/data-gen/Model-Params/
-    readr::write_rds(x = THR_Params, 
-                     file = path2File)
-    usethis::ui_done("Access parameters from {usethis::ui_path(path2File)}")
+  # Subset Comparators =======================================================
+  if(isTRUE(include_NP2)) {
+    # Select Correct Survival Data
+    params <- params[names(params) != "Survival"]
+    names(params) <- 
+      sub(pattern = "Survival_NP2", replacement = "Survival", x = names(params))
     
   } else {
-    usethis::ui_info("Load parameters from {usethis::ui_path(path2File)}")
+    # Select Correct Survival Data
+    params <- params[names(params) != "Survival_NP2"]
+    # Subset Prices
+    params$Prices <- params$Prices[-3]
   }
+  
+  return(params)
 }
