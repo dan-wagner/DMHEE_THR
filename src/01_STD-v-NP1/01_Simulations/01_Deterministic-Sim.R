096540641ffc7fb976f6d5b73d30c64c07eb124b
Daniel Wagner <wagnerd58@gmail.com>
2024-05-16 08:54:06 -0600

    Revise deterministic simulations

--- src/01_STD-v-NP1/01_Simulations/01_Deterministic-Sim.R
+++ src/01_STD-v-NP1/01_Simulations/01_Deterministic-Sim.R
@@ -1,52 +1,49 @@
 # Conduct Deterministic Simulation of Cohort Model
+# Model with two alternatives: STD, NP1
 
 source(file = file.path("src", "FUNS", "Model-Parameters.R"))
 source(file = file.path("src", "FUNS", "Model-Parameters_Time-Dependencies.R"))
-source(file = file.path("src", "FUNS", "Model-Parameters_Draws.R"))
 source(file = file.path("src", "FUNS", "THR-Model.R"))
 
 # Get Model Parameters =========================================================
-getParams(include_NP2 = FALSE)
-THR_Params <- readr::read_rds(file = file.path("data", 
-                                               "data-gen", 
-                                               "Model-Params", 
-                                               "THR-Params_j2.rds"))
+sim_params <- getParams(include_NP2 = FALSE)
 
-# Estimate Costs and Effects ===================================================
-## Analysis strategy includes 6 separate scenario analyses: 
-##    - Female: Age 40, 60 (BC), 80
-##    - Male: Age 40, 60, 80
-## Instead of running each analysis separately, I can do them all in a single 
-## function call and store the output in a deliberately formatted array.
+# Execute Simulation ===========================================================
+# Draw Parameter Values --------------------------------------------------------
+param_i <- draw_params(params = sim_params, prob = FALSE)
 
+# Estimate Costs and Effects ---------------------------------------------------
+# Analysis Strateg involves 6 separate scenario analyses:
+#   - Female: Age 40, 60 (BC), 80
+#   - Male: Age 40, 60, 80
+# Instead of running each analysis separately, they can be executed in a single
+# function call. The output can then be stored in an array. 
 
-Param_0 <- DrawParams(ParamList = THR_Params, prob = 0)
-
-simResult <- 
-  sapply(X = c("40" = 40, "60" = 60, "80" = 80), 
-         FUN = \(age){
-           sapply(X = c(Male = "Male", Female = "Female"), 
+sim_out <- 
+  sapply(X = c("40" = 40, "60" = 60, "80" = 80),
+         FUN = \(age) {
+           sapply(X = c("Male" = "Male", "Female" = "Female"),
                   FUN = \(sex){
-                    sapply(X = c(STD = "STD", NP1 = "NP1"), 
-                           FUN = runModel, 
-                           ParamList = Param_0, 
-                           Age0 = age, 
+                    sapply(X = c("STD" = "STD", "NP1" = "NP1"), 
+                           FUN = runModel,
+                           ParamList = param_i,
+                           Age0 = age,
                            Gender = sex, 
-                           nCycles = 60, 
-                           cDR = 0.06, 
-                           oDR = 0.015, 
-                           simplify = TRUE)
-                    }, 
+                           n_cycles = 60,
+                           n_cohort = 1000,
+                           cDR = 0.06,
+                           oDR = 0.015)
+                  },
                   simplify = "array")
-           }, 
+         }, 
          simplify = "array")
 
 # Name and Re-arrange array dimensions. 
-names(dimnames(simResult)) <- c("Result", "j", "Gender", "Age")
-simResult <- aperm(a = simResult, perm = c("j", "Result", "Gender", "Age"))
+names(dimnames(sim_out)) <- c("Result", "j", "Gender", "Age")
+sim_out <- aperm(a = sim_out, perm = c("j", "Result", "Gender", "Age"))
 
 # Save Output ==================================================================
-readr::write_rds(x = simResult, 
+readr::write_rds(x = sim_out, 
                  file = file.path("data", 
                                   "data-gen", 
                                   "Simulation-Output", 
